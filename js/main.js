137562730ea54c988b26908039c15494bf5ddb29
davidabutler92 <davidabutler92@yahoo.com>
2020-04-08 14:54:49 -0700

    add roll function to tally roundtotal and add to players overall score

--- js/main.js
+++ js/main.js
@@ -5,8 +5,23 @@ const Player = function(name, score, playerTurn) {
   this.playerTurn = playerTurn;
 }
 
-const playerOne = {};
-const playerTwo = {};
+const endTurn = () => {
+  
+}
+
+let rolled = 0;
+let roundTotal = 0;
+const roll = () => {
+  rolled = Math.floor(Math.random()*6)+1;
+  if(roundTotal !== 1) {
+    roundTotal = roundTotal + rolled;
+  } else {
+    roundTotal = 0;
+  }
+}
+
+let playerOne = {};
+let playerTwo = {};
 
 //UI logic
 $(document).ready(function() {
@@ -14,8 +29,22 @@ $(document).ready(function() {
     event.preventDefault();
     const player1Name = $("#player1").val();
     const player2Name = $("#player2").val();
+    playerOne = new Player(player1Name, 0, true);
+    playerTwo = new Player(player2Name, 0, false);
 
     $("#sign-in").hide();
     $("#pig-dice").show();
+    $("#player-one-name").text(playerOne.name);
+    $("#player-one-score").text(playerOne.score);
+    $("#player-two-name").text(playerTwo.name);
+    $("#player-two-score").text(playerTwo.score);
+
+    $("#roll-button").click(function() {
+    
+    });
+    $("#hold-button").click(function() {
+
+    }); 
+
   });
 });
